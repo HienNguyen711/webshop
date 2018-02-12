2de13b18e3fb1000991b0fa4fd4acb803ca66d73
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-02-12 19:32:36 +0200

    update dependencies

---
+++ src/main/java/project/webshop/model/Product.java
@@ -0,0 +1,9 @@
+package project.webshop.model;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+public class Product {
+}
---
+++ src/main/java/project/webshop/utils/CommonUtils.java
@@ -0,0 +1,4 @@
+package project.webshop.utils;
+
+public class CommonUtils {
+}
